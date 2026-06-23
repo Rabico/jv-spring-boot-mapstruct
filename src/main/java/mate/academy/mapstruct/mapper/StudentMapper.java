1271ce3b16b47d19d0b4c0ea6655db68911df4be
Przemysław Ciżmiński <cizminski@interia.pl>
2026-06-23 20:00:58 +0200

    Fourth try

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -4,14 +4,21 @@ import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
 import mate.academy.mapstruct.model.Student;
+import mate.academy.mapstruct.model.Subject;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(uses = {GroupMapper.class, SubjectMapper.class})
 public interface StudentMapper {
-    @Mapping(source ="subjects.id", target = "subjectIds")
+    @Mapping(source ="subjects", target = "subjectIds", qualifiedByName = "subjectToId")
     @Mapping(source ="group.id", target = "groupId")
     StudentDto toDto(Student student);
+
+    @Named("subjectToId")
+    default Long subjectToId(Subject subject) {
+        return subject.getId();
+    }
 
     @Mapping(source ="group.id", target = "groupId")
     StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
